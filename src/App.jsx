ef88cb0feec16960e590333b6c0bcf660345be8c
Charlton Tramel <ctramel14@gmail.com>
2024-08-19 15:51:37 -0500

    CSS

--- src/App.jsx
+++ src/App.jsx
@@ -5,16 +5,30 @@ import './App.css'
 function App() {
 
   const [puppies, setPuppies] = useState(puppyList);
+  const [featPupId, setFeatPupId] = useState(null)
 
   console.log("puppyList: ", puppyList);
+  const featuredPup = puppies.find((pup)=> pup.id === featPupId)
+  console.log(featuredPup);
+    
 
   return (
     <div className="App">
+      <h1>Puppy Pals</h1>
       {
         puppies.map((puppy) => {
-             return <p key={puppy.id}>{puppy.name}</p>;
+             return <p onClick={()=>{console.log(setFeatPupId(puppy.id))}} key={puppy.id}>{puppy.name}</p>;
            })
        }
+       {featPupId && (
+        <div>
+          <h2>{featuredPup.name}!</h2>
+          <ul>
+            <li>Age: {featuredPup.age}</li>
+            <li>Email: {featuredPup.email}</li>
+          </ul>
+        </div>
+      )}
     </div>
   );
 }
